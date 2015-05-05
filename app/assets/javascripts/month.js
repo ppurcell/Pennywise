9e3f3e12e7d97bedfe99b01e4a7706a9a884ad29
Peter Purcell <nchotstff@gmail.com>
2015-05-05 16:52:35 -0500

    All Fields now editable in month table

--- app/assets/javascripts/month.js
+++ app/assets/javascripts/month.js
@@ -16,42 +16,92 @@ var MonthTableGen =
             }
         );
 
-        $('#ledger_table').on( 'page.dt', function () {
-            var info = table.page.info();
+        $('#ledger_table').on('page.dt', function () {
+            //var info = table.page.info();
             //alert( 'Showing page: '+info.page+' of '+info.pages );
-            checkIn();
-        } );
+        });
     }
 
 }
 
-var MonthTableGui =
+var gui = function() {}
+
+gui.prototype.init = function()
 {
-    registerInlineEdit: function () {
+    var _this = this;
         $('.inline-edit-btn').click(function () {
             var parentRow = $(this).closest('tr');
+            var rowId = parentRow.attr('data-row-id');
 
             if (parentRow.hasClass('currently-editing')) {
-                var dateValue = parentRow.find('#temp-date-picker').val();
-                $('#ledger_table').DataTable().row(parentRow).invalidate().draw();
+                //Date
+                var dateValue = parentRow.find('#date-picker-' + rowId).val();
+                parentRow.find('.entry_date').html(dateValue);
+                //Description
+                var descValue = parentRow.find('#gen-desc-input-' + rowId).val();
+                parentRow.find('.entry_desc').html(descValue);
+                //Amount
+                var amtValue = parentRow.find('#gen-amt-input-' + rowId).val();
+                parentRow.find('.entry_amt').html(amtValue);
+
+                $('#ledger_table').DataTable().row(parentRow).invalidate();
                 parentRow.removeClass("currently-editing");
             }
             else {
                 parentRow.addClass("currently-editing");
+                //Date
                 var dateValue = parentRow.find('.entry_date').html();
-
-                var dateTemplate =  HandlebarsTemplates['month/date_form']({entry_date: dateValue});
+                var dateTemplate = HandlebarsTemplates['month/date_form']({row_id: rowId, entry_date: dateValue});
                 parentRow.find('.entry_date').html(dateTemplate);
-                $("#temp-date-picker").datepicker();
+                $(".date-picker").datepicker();
+                //Description
+                var descCell = parentRow.find('.entry_desc');
+                var descTemplate = HandlebarsTemplates['month/input_form']({
+                    row_id: rowId,
+                    value: descCell.html(),
+                    input_type: 'desc'
+                });
+                descCell.html(descTemplate);
+                //Amount
+                var amtCell = parentRow.find('.entry_amt');
+                var descTemplate = HandlebarsTemplates['month/input_form']({
+                    row_id: rowId,
+                    value: amtCell.html(),
+                    input_type: 'amt'
+                });
+                amtCell.html(descTemplate);
+                //Categories
+                _this.loadCategories(rowId, parentRow);
+
             }
         });
+}
 
-    }
+gui.prototype.loadCategories = function(rowId, parentRow)
+{
+
+    var selectValue = parentRow.find('.entry_cat').attr('data-category-id');
+
+    $.ajax(
+        {
+            url: '/categories.json',
+            Accept: 'application/json',
+            dataType: 'json'
+        }).success(function (data) {
+            //alert('asd');
+            //alert(HandlebarsTemplates['month/category_form'](data));
+            var categoryList = HandlebarsTemplates['month/category_form'](data)
+            parentRow.find('.entry_cat').html(categoryList);
+            parentRow.find('select').val(selectValue);
+
+        })
+
 }
 
 $(document).ready(function () {
         var dTable = MonthTableGen;
-        MonthTableGui.registerInlineEdit();
+        var tableGUI = new gui();
+        tableGUI.init();
         dTable.init();
 
     }
