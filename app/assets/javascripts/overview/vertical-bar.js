f85fa5b56031987e8b33023ba48be7cfa073204d
Peter Purcell <nchotstff@gmail.com>
2015-05-13 08:09:09 -0500

    Minor BugFixes
    - Added Overview Journal Entry Check
    - Added vertical-bar jquery check
    - Fixed Title Anchor

--- app/assets/javascripts/overview/vertical-bar.js
+++ app/assets/javascripts/overview/vertical-bar.js
@@ -1,33 +1,32 @@
 google.setOnLoadCallback(drawStuff);
-google.load("visualization", '1.1', {packages:['corechart, geochart']});
-
+google.load("visualization", '1.1', {packages: ['corechart, geochart']});
 
 
 function drawStuff() {
     var barStyle = 'bar{stroke-width:1; stroke-color:black}';
     var data = google.visualization.arrayToDataTable([
-        ['Category', 'Actual',{ role: 'style' },'Estimated', { role: 'style' }],
-        ['Travel', 10,barStyle, 1391, barStyle ],
-        ['Housing', 1253,barStyle, 2200, barStyle ],
-        ['Food', 563.00,barStyle, 1900, barStyle  ],
-        ['Souvenirs', 563.00,barStyle, 1900, barStyle  ],
-        ['Misc', 563.00,barStyle, 1900, barStyle  ]
+        ['Category', 'Actual', {role: 'style'}, 'Estimated', {role: 'style'}],
+        ['Travel', 10, barStyle, 1391, barStyle],
+        ['Housing', 1253, barStyle, 2200, barStyle],
+        ['Food', 563.00, barStyle, 1900, barStyle],
+        ['Souvenirs', 563.00, barStyle, 1900, barStyle],
+        ['Misc', 563.00, barStyle, 1900, barStyle]
     ]);
 
     var pie_data = google.visualization.arrayToDataTable([
         ['Task', 'Hours per Day'],
-        ['Travel',     11],
-        ['Housing',      2],
-        ['Food',  2],
+        ['Travel', 11],
+        ['Housing', 2],
+        ['Food', 2],
         ['Souvenirs', 2],
-        ['Misc',    7]
+        ['Misc', 7]
     ]);
 
     var geo_data = google.visualization.arrayToDataTable([
-        ['Location',   'Money Spent', 'Percentage'],
-        ['Rotorua',  312,5],
-        ['Cairns', 212,5],
-        ['Waiheke Island',  100,5],
+        ['Location', 'Money Spent', 'Percentage'],
+        ['Rotorua', 312, 5],
+        ['Cairns', 212, 5],
+        ['Waiheke Island', 100, 5],
         ['Franz Josef Glacier', 120, 5],
         ['Queenstown', 100, 5],
         ['Wellington', 100, 5],
@@ -36,11 +35,11 @@ function drawStuff() {
     ]);
 
     var geo_options = {
-        sizeAxis: { minValue: 0, maxValue: 100 },
+        sizeAxis: {minValue: 0, maxValue: 100},
         region: '053', // Western Europe
         displayMode: 'markers',
         colorAxis: {colors: ['#e7711c', '#4374e0']}, // orange to blue
-        height:240,
+        height: 240,
         'chartArea': {'width': '90%', 'height': '80%'}
     };
 
@@ -52,14 +51,14 @@ function drawStuff() {
     var options_fullStacked = {
         isStacked: 'percent',
         height: 300,
-        colors:['#ECDD4C','#8CEC7A'],
+        colors: ['#ECDD4C', '#8CEC7A'],
         legend: {
-            position:'none'
+            position: 'none'
         },
         'chartArea': {'width': '80%', 'height': '80%'},
         bar: {groupWidth: "66%"},
         hAxis: {
-            gridlines:{count:0},
+            gridlines: {count: 0},
             minValue: 0
 
         },
@@ -75,8 +74,10 @@ function drawStuff() {
 };
 
 $(document).ready(function () {
-    $(window).resize(function(){
+    if ($('#piechart').length > 0 && $('#dual_x_div').length > 0 && $('#geo_chart').length > 0) {
+        $(window).resize(function () {
+            drawStuff();
+        });
         drawStuff();
-    });
-    drawStuff();
+    }
 });
